52d500582dac2a2a2499c191f89b360f516ecba7
erlinaptiwi <80669712+erlinaptiwi@users.noreply.github.com>
2021-10-17 00:42:40 +0700

    Add files via upload

---
+++ Assets/script/mainmenuscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenuscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void mulaigame()
+    {
+        SceneManager.LoadScene("puzzel");
+    }
+
+    public void kembalikemenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    // Update is called once per frame
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+}
